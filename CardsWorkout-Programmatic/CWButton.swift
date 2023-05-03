a9f2ac0f02ef9bef0b3b6f8b7629edc59284bbd4
Yulian Gyurov <julian.gyuroff@gmail.com>
2023-05-03 20:40:13 +0300

    Add files via upload

---
+++ CardsWorkout-Programmatic/CWButton.swift
@@ -0,0 +1,35 @@
+//
+//  CWButton.swift
+//  CardsWorkout-Programmatic
+//
+//  Created by Yulian Gyuroff on 30.04.23.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false   // means Use Autolayout
+    }
+
+}
